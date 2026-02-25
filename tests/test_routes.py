89c2e9e1403368e32e6e2c8664350d9bbd0dad77
Kevin Zamora <kevin.zamora.amela@gmail.com>
2026-02-25 06:37:11 -0500

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -235,3 +235,11 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+    # CORS
+    def test_cors_security(self):
+            """It should return a CORS header"""
+            response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            # Verificamos que la cabecera CORS permita todos los orígenes (*)
+            self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
